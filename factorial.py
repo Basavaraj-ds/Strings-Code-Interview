ababf89421e3fa245f520757d200e9a20d7c5929
Basavaraj-ds <59473559+Basavaraj-ds@users.noreply.github.com>
2025-11-12 10:41:45 +0530

    factorial.py

---
+++ factorial.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    fact=1
+    for i in range(1,x):
+        if x==0:
+            print("fact of 0 is 1")
+        else:
+            fact=fact*i
+            print(fact)
+            
+n=int(input("enter a number="))
+factorial(n)
